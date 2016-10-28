4281585ece214cb4ff7aa1bca7f56015c080863b
Dzmitry_Ramaniuk2 <Dzmitry_Ramaniuk2@epam.com>
2016-10-28 18:08:03 +0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// entry: {
+//     app: 'src/app.ts',
+//         vendor: 'src/vendor.ts'
+// },
+//
+// output: {
+//     filename: '[name].js'
+// },
+//
+// loaders: [
+//     {
+//         test: /\.ts$/
+//         loaders: 'ts'
+//     },
+//     {
+//         test: /\.css$/
+//         loaders: 'style!css'
+//     }
+// ]
